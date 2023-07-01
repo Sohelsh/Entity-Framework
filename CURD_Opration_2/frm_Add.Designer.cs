4642d15cf27d4dfd181cb6da72127543707a2e2c
Sohelsh <sohelshaikhadam@gmail.com>
2023-07-01 19:36:20 +0530

    Entity CRUD

---
+++ CURD_Opration_2/frm_Add.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace CURD_Opration_2
+{
+    partial class frm_Add
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Head = new System.Windows.Forms.Label();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.lbl_Gender = new System.Windows.Forms.Label();
+            this.lbl_Age = new System.Windows.Forms.Label();
+            this.lbl_Class = new System.Windows.Forms.Label();
+            this.tb_Name = new System.Windows.Forms.TextBox();
+            this.cmb_Gender = new System.Windows.Forms.ComboBox();
+            this.tb_Age = new System.Windows.Forms.TextBox();
+            this.tb_Class = new System.Windows.Forms.TextBox();
+            this.btn_Insert = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.lbl_Id = new System.Windows.Forms.Label();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_Head
+            // 
+            this.lbl_Head.AutoSize = true;
+            this.lbl_Head.Font = new System.Drawing.Font("Lucida Sans", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Head.Location = new System.Drawing.Point(499, 3);
+            this.lbl_Head.Name = "lbl_Head";
+            this.lbl_Head.Size = new System.Drawing.Size(218, 38);
+            this.lbl_Head.TabIndex = 0;
+            this.lbl_Head.Text = "Add Student";
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Name.Location = new System.Drawing.Point(264, 106);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(90, 32);
+            this.lbl_Name.TabIndex = 1;
+            this.lbl_Name.Text = "Name";
+            // 
+            // lbl_Gender
+            // 
+            this.lbl_Gender.AutoSize = true;
+            this.lbl_Gender.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Gender.Location = new System.Drawing.Point(264, 168);
+            this.lbl_Gender.Name = "lbl_Gender";
+            this.lbl_Gender.Size = new System.Drawing.Size(110, 32);
+            this.lbl_Gender.TabIndex = 1;
+            this.lbl_Gender.Text = "Gender";
+            // 
+            // lbl_Age
+            // 
+            this.lbl_Age.AutoSize = true;
+            this.lbl_Age.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Age.Location = new System.Drawing.Point(264, 236);
+            this.lbl_Age.Name = "lbl_Age";
+            this.lbl_Age.Size = new System.Drawing.Size(66, 32);
+            this.lbl_Age.TabIndex = 1;
+            this.lbl_Age.Text = "Age";
+            // 
+            // lbl_Class
+            // 
+            this.lbl_Class.AutoSize = true;
+            this.lbl_Class.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Class.Location = new System.Drawing.Point(264, 299);
+            this.lbl_Class.Name = "lbl_Class";
+            this.lbl_Class.Size = new System.Drawing.Size(86, 32);
+            this.lbl_Class.TabIndex = 1;
+            this.lbl_Class.Text = "Class";
+            // 
+            // tb_Name
+            // 
+            this.tb_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Name.Location = new System.Drawing.Point(696, 104);
+            this.tb_Name.Name = "tb_Name";
+            this.tb_Name.Size = new System.Drawing.Size(350, 34);
+            this.tb_Name.TabIndex = 2;
+            // 
+            // cmb_Gender
+            // 
+            this.cmb_Gender.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_Gender.FormattingEnabled = true;
+            this.cmb_Gender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cmb_Gender.Location = new System.Drawing.Point(696, 163);
+            this.cmb_Gender.Name = "cmb_Gender";
+            this.cmb_Gender.Size = new System.Drawing.Size(350, 37);
+            this.cmb_Gender.TabIndex = 3;
+            // 
+            // tb_Age
+            // 
+            this.tb_Age.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Age.Location = new System.Drawing.Point(696, 234);
+            this.tb_Age.Name = "tb_Age";
+            this.tb_Age.Size = new System.Drawing.Size(350, 34);
+            this.tb_Age.TabIndex = 2;
+            // 
+            // tb_Class
+            // 
+            this.tb_Class.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Class.Location = new System.Drawing.Point(696, 299);
+            this.tb_Class.Name = "tb_Class";
+            this.tb_Class.Size = new System.Drawing.Size(350, 34);
+            this.tb_Class.TabIndex = 2;
+            // 
+            // btn_Insert
+            // 
+            this.btn_Insert.BackColor = System.Drawing.Color.LightSalmon;
+            this.btn_Insert.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Insert.Location = new System.Drawing.Point(253, 367);
+            this.btn_Insert.Name = "btn_Insert";
+            this.btn_Insert.Size = new System.Drawing.Size(145, 53);
+            this.btn_Insert.TabIndex = 4;
+            this.btn_Insert.Text = "Insert";
+            this.btn_Insert.UseVisualStyleBackColor = false;
+            this.btn_Insert.Click += new System.EventHandler(this.btn_Insert_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.BackColor = System.Drawing.Color.LightSalmon;
+            this.btn_Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Update.Location = new System.Drawing.Point(487, 367);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(145, 53);
+            this.btn_Update.TabIndex = 4;
+            this.btn_Update.Text = "Update";
+            this.btn_Update.UseVisualStyleBackColor = false;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.BackColor = System.Drawing.Color.LightSalmon;
+            this.btn_Reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Reset.Location = new System.Drawing.Point(901, 367);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(145, 53);
+            this.btn_Reset.TabIndex = 4;
+            this.btn_Reset.Text = "Reset";
+            this.btn_Reset.UseVisualStyleBackColor = false;
+            this.btn_Reset.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.BackColor = System.Drawing.Color.LightSalmon;
+            this.btn_Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Delete.Location = new System.Drawing.Point(696, 367);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(145, 53);
+            this.btn_Delete.TabIndex = 4;
+            this.btn_Delete.Text = "Delete";
+            this.btn_Delete.UseVisualStyleBackColor = false;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(89, 435);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(1119, 362);
+            this.dataGridView.TabIndex = 5;
+            this.dataGridView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView_MouseClick);
+            // 
+            // lbl_Id
+            // 
+            this.lbl_Id.AutoSize = true;
+            this.lbl_Id.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Id.Location = new System.Drawing.Point(264, 66);
+            this.lbl_Id.Name = "lbl_Id";
+            this.lbl_Id.Size = new System.Drawing.Size(42, 32);
+            this.lbl_Id.TabIndex = 1;
+            this.lbl_Id.Text = "ID";
+            // 
+            // tb_id
+            // 
+            this.tb_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_id.Location = new System.Drawing.Point(696, 64);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(350, 34);
+            this.tb_id.TabIndex = 2;
+            // 
+            // frm_Add
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.ClientSize = new System.Drawing.Size(1266, 818);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Reset);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Insert);
+            this.Controls.Add(this.cmb_Gender);
+            this.Controls.Add(this.tb_Class);
+            this.Controls.Add(this.tb_Age);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.tb_Name);
+            this.Controls.Add(this.lbl_Class);
+            this.Controls.Add(this.lbl_Age);
+            this.Controls.Add(this.lbl_Id);
+            this.Controls.Add(this.lbl_Gender);
+            this.Controls.Add(this.lbl_Name);
+            this.Controls.Add(this.lbl_Head);
+            this.Name = "frm_Add";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Student";
+            this.Load += new System.EventHandler(this.frm_Add_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Head;
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.Label lbl_Gender;
+        private System.Windows.Forms.Label lbl_Age;
+        private System.Windows.Forms.Label lbl_Class;
+        private System.Windows.Forms.TextBox tb_Name;
+        private System.Windows.Forms.ComboBox cmb_Gender;
+        private System.Windows.Forms.TextBox tb_Age;
+        private System.Windows.Forms.TextBox tb_Class;
+        private System.Windows.Forms.Button btn_Insert;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Reset;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label lbl_Id;
+        private System.Windows.Forms.TextBox tb_id;
+    }
+}
+
---
+++ CURD_Opration_2/frm_Add.cs
@@ -0,0 +1,131 @@
+﻿using CURD_Opration_2.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CURD_Opration_2
+{
+    public partial class frm_Add : Form
+    {
+        student sobj = new student();
+        CRUD_TwoEntities Dbobj = new CRUD_TwoEntities();
+        int N_Id = 0;
+        public frm_Add()
+        {
+            InitializeComponent();
+        }
+
+        void Show_Data()
+        {
+            dataGridView.DataSource = Dbobj.students.ToList<student>();
+        }
+
+        private void frm_Add_Load(object sender, EventArgs e)
+        {
+            Show_Data();
+        }
+
+        void Clear_Controls()
+        {
+            tb_Name.Clear();
+            tb_Age.Clear();
+            tb_Class.Clear();
+            cmb_Gender.SelectedItem = null;
+        }
+
+        private void btn_Insert_Click(object sender, EventArgs e)
+        {
+            sobj.name = tb_Name.Text.Trim();
+            sobj.gender = cmb_Gender.SelectedItem.ToString();
+            sobj.standard = Convert.ToInt32(tb_Class.Text.Trim());
+            sobj.age = Convert.ToInt32(tb_Age.Text.Trim());
+            Dbobj.students.Add(sobj);
+            int Ans = Dbobj.SaveChanges();
+
+            if(Ans > 0)
+            {
+                MessageBox.Show("Data Insert Successfully", "", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                Show_Data();
+                Clear_Controls();
+            }
+            else
+            {
+                MessageBox.Show("Data Insertion Failed", "", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {
+            Clear_Controls();
+        }
+
+        private void dataGridView_MouseClick(object sender, MouseEventArgs e)
+        {
+            N_Id = Convert.ToInt32(dataGridView.SelectedRows[0].Cells[0].Value);
+            sobj = Dbobj.students.Where(x => x.id == N_Id).FirstOrDefault();
+            tb_Name.Text = sobj.name;
+            cmb_Gender.SelectedItem = sobj.gender;
+            tb_Age.Text = sobj.age.ToString();
+            tb_Class.Text = sobj.standard.ToString();
+
+        }
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            sobj.id = N_Id;
+            sobj.name = tb_Name.Text.Trim();
+            sobj.gender = cmb_Gender.SelectedItem.ToString();
+            sobj.standard = Convert.ToInt32(tb_Class.Text.Trim());
+            sobj.age = Convert.ToInt32(tb_Age.Text.Trim());
+            Dbobj.Entry(sobj).State = System.Data.Entity.EntityState.Modified;
+
+            int Ans = Dbobj.SaveChanges();
+
+            if (Ans > 0)
+            {
+                MessageBox.Show("Data Update Successfully", "", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                Show_Data();
+                Clear_Controls();
+            }
+            else
+            {
+                MessageBox.Show("Data Updation Failed", "", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Are You Sure to delete data","Alert",MessageBoxButtons.YesNo,MessageBoxIcon.Warning);
+
+            if(dr == DialogResult.Yes)
+            {
+                sobj = Dbobj.students.Where(x => x.id == N_Id).FirstOrDefault();
+                Dbobj.Entry(sobj).State = EntityState.Deleted;
+
+                int Ans = Dbobj.SaveChanges();
+
+                if (Ans > 0)
+                {
+                    MessageBox.Show("Data Deleted Successfully", "", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                    Show_Data();
+                    Clear_Controls();
+                }
+                else
+                {
+                    MessageBox.Show("Data Deleted Failed", "", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("You have cancelled the deleted opration", "", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
